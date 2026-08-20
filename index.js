a07880ef2867587d5a103be54f8009bc99539f56
Camillaneri <camilla.neri7@studio.unibo.it>
2026-08-20 18:45:15 +0200

    y

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ let score = 0;
 
 document.querySelector(".score").textContent = score;
 
-fetch(".\data\cards.json")
+fetch("C:\Users\Camilla\Desktop\TESI - imd\SCROLLYTELL\memory\memoryGame\data\card.json")
   .then((res) => res.json())
   .then((data) => {
     cards = [...data, ...data];
