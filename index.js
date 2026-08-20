8f8a75fd767ccb7f648216ce6c75f4a2e3219d3b
Camillaneri <camilla.neri7@studio.unibo.it>
2026-08-20 18:43:06 +0200

    t

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ let score = 0;
 
 document.querySelector(".score").textContent = score;
 
-fetch("data\card.json")
+fetch(".\data\cards.json")
   .then((res) => res.json())
   .then((data) => {
     cards = [...data, ...data];
